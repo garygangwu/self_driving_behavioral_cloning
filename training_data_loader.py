05a1fa27368f3e960f72df2e4e4b8a1f73f07073
Gary Wu <gary.wu@airbnb.com>
2017-10-21 19:52:11 -0700

    update the loader

--- training_data_loader.py
+++ training_data_loader.py
@@ -4,14 +4,22 @@ import matplotlib.image as mpimg
 import numpy as np
 
 driving_log_file = 'data/driving_log.csv'
+image_fold = 'data/IMG'
 
 def load_data():
-  reader = csv.DictReader(open(driving_log_file))
+  reader = csv.reader(open(driving_log_file))
   X = []
   y = []
   for row in reader:
-    img_file_name = 'data/' + row['center'].strip()
-    img = mpimg.imread(img_file_name)
+    center_img_file_name = image_fold + row[0].split('/')[-1]
+    left_img_file_name   = image_fold + row[1].split('/')[-1]
+    right_img_file_name  = image_fold + row[2].split('/')[-1]
+    steering = float(row[3])
+    throttle = float(row[4])
+    brake    = float(row[5])
+    speed    = float(row[6])
+
+    img = mpimg.imread(center_img_file_name)
     X.append(img)
-    y.append(float(row['steering'].strip()))
+    y.append(steering)
   return np.array(X), np.array(y)
