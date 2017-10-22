aa56bb77227e6739385ce9507c33bf8a8ee2c588
Gary Wu <mr.gang.wu@gmail.com>
2017-10-22 07:16:46 +0000

    updated architecture with based on nvda's paper

--- train.py
+++ train.py
@@ -2,42 +2,35 @@ import tensorflow as tf
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D, Merge
+from keras.layers import Conv2D, MaxPooling2D, Merge, Lambda, Cropping2D
 from keras.utils import np_utils
+#from keras.callbacks import ModelCheckpoint
 from training_data_loader import *
 
 model_file = 'model.h5'
 
-# left_branch = Sequential()
-# left_branch.add(Conv2D(32, (3, 3), activation='relu', input_shape=(160, 320, 3)))
-
-# middle_branch = Sequential()
-# middle_branch.add(Conv2D(32, (3, 3), activation='relu', input_shape=(160, 320, 3)))
-
-# right_branch = Sequential()
-# right_branch.add(Conv2D(32, (3, 3), activation='relu', input_shape=(160, 320, 3)))
-
-# merged = Merge([left_branch, middle_branch, right_branch], mode='concat')
-
-# final_model = Sequential()
-# final_model.add(merged)
-# final_model.add(Dense(10, activation='softmax'))
-
 model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(160, 320, 3)))
-model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((50,20), (0,0))))
+model.add(Conv2D(24, (5, 5), activation='elu', subsample=(2,2)))
+model.add(Conv2D(36, (5, 5), activation='elu', subsample=(2,2)))
+model.add(Conv2D(48, (5, 5), activation='elu', subsample=(2,2)))
+model.add(Conv2D(64, (5, 5), activation='elu'))
+#model.add(Conv2D(64, (5, 5), activation='elu'))
 model.add(Dropout(0.5))
-model.add(Activation('relu'))
 model.add(Flatten())
-model.add(Dense(128))
-model.add(Activation('relu'))
-model.add(Dense(10))
-model.add(Activation('relu'))
+model.add(Dense(100, activation='elu'))
+model.add(Dense(50, activation='elu'))
+model.add(Dense(10, activation='elu'))
 model.add(Dense(1))
+model.summary()
 
+#checkpoint = ModelCheckpoint('model-{epoch:03d}.h5', monitor='val_loss', verbose=0, save_best_only=True, mode='auto')
 model.compile(loss='mean_squared_error', optimizer='adam')
 
 X_train, y_train = load_data()
 model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
 model.save(model_file)
 
+l = model.predict(X_train, batch_size=100)
+print(l)
